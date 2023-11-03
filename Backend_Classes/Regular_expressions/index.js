95c9bde535b2ac1f223c8f8d1886849c8ad457a2
muskanthapa2000 <7022muskan@gmail.com>
2023-11-03 23:35:22 +0530

    d

---
+++ Backend_Classes/Regular_expressions/index.js
@@ -0,0 +1,9 @@
+
+const regularexpression1 = /./
+const regularexpression2 = (".")
+
+// . -> any single character 
+const string = "a";
+
+const result = regularexpression1.test(string);
+console.log(result);
