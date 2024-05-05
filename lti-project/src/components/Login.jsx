7cfe44831f619aa51eaa9ff80e4454a269902428
sourav <sourav.samanta@hhstechgroup.com>
2024-05-05 23:23:16 +0530

    d

---
+++ lti-project/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Login(props) {
+  return (
+    <div>
+      <h1 className="text-gray-50">LOGIN PAGE</h1>
+    </div>
+  )
+}
+
+
+export default Login
+
---
+++ lti-project/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/components/Login.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
